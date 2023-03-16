2bbe2ce2ff641de7deded9f4d56c9e2db5658d70
Dikhnich Stepan <stepan.dihnich@mail.ru>
2023-03-16 12:03:47 +0300

    .

---
+++ SubscriptionEditionsPrEP/SubscriptionEditionsPrEP/Models/Recipients.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionEditionsPrEP.Models
+{
+    public class Recipients
+    {
+        public int recipient_id;
+        public string surname;
+        public string name;
+        public string patronymic;
+        public string address;
+        public string login;
+        public string password;
+        public string Image;
+        public bool isAdmin;
+
+        public Recipients(int recipient_id, string surname, string name, string patronymic, string address,
+            string login, string password, string Image, bool isAdmin)
+        {
+            this.recipient_id = recipient_id;
+            this.surname = surname;
+            this.name = name;
+            this.patronymic = patronymic;
+            this.address = address;
+            this.login = login;
+            this.password = password;
+            this.Image = Image;
+            this.isAdmin = isAdmin;
+        }
+    }
+}
---
+++ SubscriptionEditionsPrEP/SubscriptionEditionsPrEP/WPF/Authorization.xaml.cs
@@ -0,0 +1,52 @@
+﻿using SubscriptionEditionsPrEP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SubscriptionEditionsPrEP.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Просмотр и скрытие пароля
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void aPicture_MouseDown(object sender, RoutedEventArgs e)
+        {
+            if (PassTextBox.Visibility == Visibility.Hidden)
+            {
+                PassTextBox.Text = PassBox.Password; // скопируем в TextBox из PasswordBox
+                PassTextBox.Visibility = Visibility.Visible; // TextBox - отобразить
+                PassBox.Visibility = Visibility.Hidden; // PasswordBox - скрыть
+            }
+            else
+            {
+                PassBox.Password = PassTextBox.Text; // скопируем в PasswordBox из TextBox 
+                PassTextBox.Visibility = Visibility.Hidden; // TextBox - скрыть
+                PassBox.Visibility = Visibility.Visible; // PasswordBox - отобразить
+            }
+        }
+        private void EnterButton(object sender, RoutedEventArgs e)
+        {   
+        }
+    }
+}
